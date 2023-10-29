49f41d19006d694138d6264e0c26c0b47a0dacc2
Temidayo-icon <oluwamuyiwatemidayo@gmail.com>
2023-10-29 19:26:18 +0100

    gnerate and scan qr code created done

--- lib/screens/connect/connect_screen.dart
+++ lib/screens/connect/connect_screen.dart
@@ -28,18 +28,7 @@ class Packet {
   }
 }
 
-void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ConnectScreen(),
-    );
-  }
-}
 
 class ConnectScreen extends StatefulWidget {
   @override
