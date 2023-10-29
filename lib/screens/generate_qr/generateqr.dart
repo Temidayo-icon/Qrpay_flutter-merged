0f438e19ac752f5141ced060f9367b9537e040b7
Temidayo-icon <oluwamuyiwatemidayo@gmail.com>
2023-10-29 16:31:05 +0100

    gnerate and scan qr code created done

--- lib/screens/generate_qr/generateqr.dart
+++ lib/screens/generate_qr/generateqr.dart
@@ -1,14 +1,20 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 
 class GenBarcodeScreen extends StatefulWidget {
-  const GenBarcodeScreen({Key? key});
+  final String myQR;
+  const GenBarcodeScreen(this.myQR, {super.key});
+
 
   static const String routeName = '/generateBarcode';
 
-  static Route route() {
+  Route route() {
     return MaterialPageRoute(
-      builder: (_) => const GenBarcodeScreen(),
+      builder: (_) =>  GenBarcodeScreen(myQR),
       settings: const RouteSettings(name: routeName),
     );
   }
@@ -19,11 +25,84 @@ class GenBarcodeScreen extends StatefulWidget {
 }
 
 class _GenBarcodeScreenState extends State<GenBarcodeScreen> {
+  TextEditingController dataController = TextEditingController();
+  TextEditingController userIdController = TextEditingController();
+  TextEditingController pinController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
+  bool isQRCodeGenerated = false;
+  String qrCodeData = '';
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  return Scaffold(
+  appBar: AppBar(
+  title: Text('Generate QR Code'),
+  ),
+  body: Padding(
+  padding: const EdgeInsets.all(16.0),
+  child: Column(
+  children: <Widget>[
+  TextField(
+  controller: dataController,
+  decoration: InputDecoration(labelText: 'Username'),
+  ),
+  TextField(
+  controller: userIdController,
+  decoration: InputDecoration(labelText: 'User ID'),
+  ),
+  TextField(
+  controller: pinController,
+  decoration: InputDecoration(labelText: 'PIN'),
+  ),
+  TextField(
+  controller: amountController,
+  decoration: InputDecoration(labelText: 'Amount'),
+  ),
+  ElevatedButton(
+  onPressed: () {
+  generateQRCode(qrCodeData, 200, 200);
+  },
+  child: Text(isQRCodeGenerated ? 'Regenerate QR Code' : 'Generate QR Code'),
+  ),
+  if (isQRCodeGenerated)
+  Column(
+  children: [
+  Text('QR Code Data: $qrCodeData'),
+  Image.memory(Uint8List.fromList(generateQRCode(qrCodeData, 200, 200))),
+  ],
+  ),
+  ],
+  ),
+  ),
+  );
   }
-  
-  
-}
+
+  generateQRCode(String qrCodeData, int i, int d) {
+  final data = dataController.text;
+  final userId = userIdController.text;
+  final pin = pinController.text;
+  final amount = amountController.text;
+
+  // Check if all fields are filled
+  if (data.isEmpty || userId.isEmpty || pin.isEmpty || amount.isEmpty) {
+  // Display an error message or toast
+  return;
+  }
+
+  // Concatenate user data into a single string
+  final userData = {
+  'username': data,
+  'userId': userId,
+  'amount': double.parse(amount),
+  };
+
+  // Create a JSON object with user information
+  final jsonData = json.encode(userData);
+
+  setState(() {
+  qrCodeData = jsonData;
+  isQRCodeGenerated = true;
+  });
+  }
+
+  }
